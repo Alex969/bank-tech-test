e4b8b1b3f12b8a99a864089abb5def1d0bf228ad
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2021-11-24 00:17:06 +0100

    test passing for error throw if trying to withdraw with insuficient funds

--- tests/account.test.js
+++ tests/account.test.js
@@ -3,21 +3,28 @@ const Account = require('../account');
 describe('account', () => {
 
   let account;
-  let amount;
+  let starterBalance;
+  let depositAmount;
+  let withdrawalAmount;
 
   beforeEach(() => {
     account = new Account();
-    depositAmount += 100;
-    withdrawalAmount -= 100;
+    starterBalance = 0;
   })
 
   it('has a balance of zero when created', () => {
-    expect(account.balance).toBe(0);
+    expect(account.balance).toBe(starterBalance);
   })
 
   it('adds money to your balance when you make a deposit', () => {
+    depositAmount += 100
     account.deposit(depositAmount);
-    expect(account.balance).toBe(0 + amount)
+    expect(account.balance).toBe(starterBalance + depositAmount)
   })
-
+  it('cannot withdraw money if balance is of 0', () => {
+    withdrawalAmount -= 100;
+    expect(() => { 
+      account.withdraw(withdrawalAmount); 
+    }).toThrow("Not enough money in your balance !");
+  })
 })
