b024c8eb65c40feaae6c903f562d60cddf24d582
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2021-11-24 10:13:09 +0100

    testing that you can withdraw from your account

--- tests/account.test.js
+++ tests/account.test.js
@@ -6,25 +6,47 @@ describe('account', () => {
   let starterBalance;
   let depositAmount;
   let withdrawalAmount;
+  let upBalance;
+  let currentBalance;
+  let downBalance;
 
   beforeEach(() => {
     account = new Account();
     starterBalance = 0;
   })
-
-  it('has a balance of zero when created', () => {
-    expect(account.balance).toBe(starterBalance);
+  describe('balance', () => {
+    it('has a balance of zero when created', () => {
+      currentBalance = account.balance;
+      expect(currentBalance).toBe(starterBalance);
+    })
+  })
+  
+  describe('deposit', () => {
+    it('adds money to your balance when you make a deposit', () => {
+      currentBalance = account.balance;
+      depositAmount = 100
+      account.deposit(depositAmount);
+      upBalance = currentBalance + depositAmount;
+      expect(account.balance).toBe(upBalance);
+    })
   })
 
-  it('adds money to your balance when you make a deposit', () => {
-    depositAmount += 100
-    account.deposit(depositAmount);
-    expect(account.balance).toBe(starterBalance + depositAmount)
-  })
-  it('cannot withdraw money if balance is of 0', () => {
-    withdrawalAmount -= 100;
-    expect(() => { 
-      account.withdraw(withdrawalAmount); 
-    }).toThrow("Not enough money in your balance !");
+  describe('withdraw', () => {
+    it('cannot withdraw money if balance is of 0', () => {
+      currentBalance = account.balance;
+      withdrawalAmount = 100;
+      expect(() => { 
+        account.withdraw(withdrawalAmount); 
+      }).toThrow("Not enough money in your balance !");
+    })
+
+    it('withdraws money from your account', () => {
+      account.balance = 90;
+      withdrawalAmount = 50;
+      downBalance = account.balance - withdrawalAmount;
+      account.withdraw(withdrawalAmount);
+      expect(account.balance).toBe(downBalance);
+
+    })
   })
 })
